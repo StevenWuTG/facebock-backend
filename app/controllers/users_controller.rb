ba65aa0184e0194d72be90fa7d1ae503158c2376
StevenWuTG <stevenwu91@icloud.com>
2020-12-28 17:29:06 -0500

    user controller done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,42 @@ class UsersController < ApplicationController
         users = User.all
         render json: users, except: [:created_at, :updated_at]
     end
+
+    def create
+        user = User.create!(user_params)
+        if user.valid?
+            render json: user,  except: [:created_at, :updated_at]
+        else
+            render json: {errors: user.errors} 
+        end 
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user, except: [:created_at, :updated_at]
+    end
+
+    def update
+        user = User.find(params[:id])
+        if (user.update!(user_params))
+            render json: user
+        else
+            render json: {errors: user.errors} 
+        end 
+    end 
+
+    def destroy
+        allUser = User.all
+        user = User.find(params[:id])
+        user.destroy
+        render json: allUser
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :vaccinated, :img_url, :first_name, :last_name, :age, :hometown)
+    end
+
     
 end
