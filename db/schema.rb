fe5a81a8b255d49d8dafa37995a50c914e700ebe
StevenWuTG <stevenwu91@icloud.com>
2021-01-04 11:19:54 -0500

    merged auth 90%

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_014837) do
+ActiveRecord::Schema.define(version: 2021_01_02_205939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2020_12_30_014837) do
     t.index ["user2_id"], name: "index_pals_on_user2_id"
   end
 
+  create_table "post_containers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_post_containers_on_post_id"
+    t.index ["user_id"], name: "index_post_containers_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "content"
@@ -76,5 +85,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_014837) do
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "messages", "users"
+  add_foreign_key "post_containers", "posts"
+  add_foreign_key "post_containers", "users"
   add_foreign_key "posts", "users"
 end
