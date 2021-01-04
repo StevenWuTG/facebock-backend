42d9932b8ee4ea7a3d7ef0fddc95f9647687e365
StevenWuTG <stevenwu91@icloud.com>
2021-01-04 17:32:47 -0500

    returning user done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,15 @@ class UsersController < ApplicationController
     end
 
     def profile
-        render json: { user: UserSerializer.new(@user)}, status: :accepted
+        render json: { user: current_user }, status: :accepted
     end
 
     def create
         # byebug
-        user = User.create!(user_params)
-        if user.valid?
-            token = encode_token(user_id: user.id)
-            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
+        @user = User.create!(user_params)
+        if @user.valid?
+            token = encode_token(user_id: @user.id)
+            render json: { user: UserSerializer.new(@user), jwt: token }, status: :created
 
         else
             render json: {errors: user.errors} 
